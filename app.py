43d77acc835b66212d0d47235f4b35e7cb1564bd
nori-pixl <sato.shigure4@gmail.com>
2026-05-20 21:26:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ TUNNEL_URL = "https://knitting-gender-dvds-hidden.trycloudflare.com"
 HTML = """
 <!DOCTYPE html><html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>ngm-mynote ver2.4 掲示板</title>
-<link rel="icon" href="icon.png" type="image/x-icon">
+<link rel="icon" href="icon.png" type="image/png">
 
 <style>
     body{font-family:monospace;background:#eee;padding:15px;color:#333;}
